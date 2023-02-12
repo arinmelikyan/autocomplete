4bdad3d5681e6b979961df1ebb1bba7eeaf4e215
Arin Melikyan <arin.melkn@gmail.com>
2023-02-12 17:24:49 +0400

    refactor

--- src/App.js
+++ src/App.js
@@ -8,36 +8,48 @@ const fetchSuggestionFromApi = async (query) => {
       `https://api.github.com/search/issues?q=${query}`
     );
     const data = await response.json();
+    if(!data.items) {
+      return [];
+    }
+    console.log('fetched', data)
     return data.items.map((item) => item.title);
   } catch(error) {
     console.log(`Error fetching data: ${error}`)
     return [];
   }
 };
-
 function App() {
   const [suggestion, setSuggestion] = useState([]);
-  const [query, setQuery] = useState("");
-  
+  const [input, setInput] = useState('');
+  const [isFetching, setIsFetching] = useState(false);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetchSuggestionFromApi(query);
-      setSuggestion(result);
+    let mounted = true;
+    if (!isFetching) {
+      return;
+    }
+    fetchSuggestionFromApi(input).then((result) => {
+      if (mounted) {
+        setSuggestion(result);
+        setIsFetching(false);
+      }
+    });
+    return () => {
+      mounted = false;
     };
-    fetchData();
-  }, [query]);
+  }, [input, isFetching]);
 
   const onInput = (e) => {
-    setQuery(e?.target?.value);
+    const query = e.target.value;
+    setInput(query);
+    setIsFetching(true);
   };
 
   return (
     <div>
       <input type="text" onInput={debounce(onInput, 500)} />
       <ul>
-        {suggestion.map((item, idx) => (
-          <li key={idx}>{item}</li>
-        ))}
+        {suggestion && suggestion.map((item, idx) => <li key={idx}>{item}</li>)}
       </ul>
     </div>
   );
